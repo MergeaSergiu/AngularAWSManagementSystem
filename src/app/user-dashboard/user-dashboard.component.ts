2123dc1205c0a37e865d908c3210ee92898fc420
MergeaSergiu <mergeasergiu@gmail.com>
2024-11-05 20:32:55 +0200

    Allow user to delete its buckets

--- src/app/user-dashboard/user-dashboard.component.ts
+++ src/app/user-dashboard/user-dashboard.component.ts
@@ -1,18 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RegistrationService } from '../services/registration.service';
+import { S3BucketService } from '../services/s3-bucket.service';
 
 @Component({
   selector: 'app-user-dashboard',
   templateUrl: './user-dashboard.component.html',
   styleUrl: './user-dashboard.component.css'
 })
-export class UserDashboardComponent {
+export class UserDashboardComponent implements OnInit {
 
-  constructor(private registrationService: RegistrationService){}
 
+  bucketList: Map<string, string> = new Map();
+  bucketListEntries: any = [];
+
+  constructor(private registrationService: RegistrationService, private s3BucketService: S3BucketService){}
+  ngOnInit(): void {
+  this.getBucketList();
+}
+
+deleteBucket(bucketName: string) {
+  this.s3BucketService.deleteBucket(bucketName).subscribe({
+        next: () =>{
+        this.getBucketList();
+    }, 
+    error: () => {}
+  })
+}
+
+getBucketList(){
+    this.s3BucketService.getBucketList().subscribe({
+      next: (response: Map<string, string>) => {
+        this.bucketList = new Map<string, string>(Object.entries(response)); // Convert object to Map
+          this.bucketListEntries =  Array.from(this.bucketList.entries());
+      }, 
+      error : (error) => {} 
+    })
+  }
+
+  
 
   logout() {
     this.registrationService.logedOut();
     }
+
+    
 
 }
